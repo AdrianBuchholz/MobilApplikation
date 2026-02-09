af5e646e61afa00d31592b8ae04ab7d6872ba83e
Adrianbuchholz <S2306770@student.hb.se>
2026-02-09 22:29:12 +0100

    fix

--- MobilApplikation/Repositories/Repository.cs
+++ MobilApplikation/Repositories/Repository.cs
@@ -1,4 +1,7 @@
-﻿using MobilApplikation.Data;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MobilApplikation.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace MobilApplikation.Repositories
@@ -10,18 +13,22 @@ namespace MobilApplikation.Repositories
         {
             _context = context;
         }
+
         public async Task<IEnumerable<T>> GetAllAsync() =>
             await _context.Set<T>().ToListAsync();
 
         public async Task<T?> GetAsync(int id) =>
             await _context.Set<T>().FindAsync(id);
 
-        public async Task AddAsync(T entity) =>
+        public async Task AddAsync(T entity)
+        {
+            // AddAsync returns a ValueTask<EntityEntry<T>>; await and ignore the result
             await _context.Set<T>().AddAsync(entity);
+        }
+
         public void Remove(T entity) =>
             _context.Set<T>().Remove(entity);
 
         public IQueryable<T> Query() => _context.Set<T>().AsQueryable();
-
     }
 }
